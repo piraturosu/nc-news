ac694302ce14c6b88b333b25224e61ce13818950
George Constantin <george.constantin33@gmail.com>
2025-03-10 14:07:47 +0000

    passed error logic to model as suggeested by Ali (thanks Ali)

--- controllers/topics.controllers.js
+++ controllers/topics.controllers.js
@@ -3,14 +3,9 @@ const { fetchAllTopics } = require("../models/topics.models");
 function getAllTopics(req, res, next) {
   fetchAllTopics()
     .then((topics) => {
-      if (topics.length === 0) {
-        return Promise.reject({ status: 404, message: "No topics found" });
-      }
       res.status(200).send({ topics });
     })
-    .catch((err) => {
-      next(err);
-    });
+    .catch(next);
 }
 
 module.exports = { getAllTopics };
--- models/topics.models.js
+++ models/topics.models.js
@@ -4,6 +4,9 @@ function fetchAllTopics() {
   return db
     .query("SELECT topics.slug, topics.description FROM topics")
     .then(({ rows }) => {
+      if (rows.length === 0) {
+        return Promise.reject({ status: 404, message: "No topics found" });
+      }
       return rows;
     });
 }
