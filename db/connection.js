86657e31dd152888a01f0589c26b93f3e77a4c59
George Constantin <george.constantin33@gmail.com>
2025-03-13 14:01:56 +0000

    seed production db

--- db/connection.js
+++ db/connection.js
@@ -4,8 +4,6 @@ const ENV = process.env.NODE_ENV || "development";
 
 require("dotenv").config({ path: `${__dirname}/../.env.${ENV}` });
 
-const db = new Pool();
-
 if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   throw new Error("No PGDATABASE configured or DATABASE_URL not set");
 } else {
