286ddc6e6b68f137c069e6e9e50fd7e232894ee2
George Constantin <george.constantin33@gmail.com>
2025-03-14 14:43:03 +0000

    ticket-16-get-api-users-username

--- models/users.models.js
+++ models/users.models.js
@@ -1,4 +1,5 @@
 const db = require("../db/connection");
+const { checkItemExists } = require("../db/seeds/utils");
 
 function fetchAllUsers() {
   return db.query("SELECT * FROM users").then(({ rows }) => {
@@ -9,4 +10,14 @@ function fetchAllUsers() {
   });
 }
 
-module.exports = { fetchAllUsers };
+function fetchUserByUsername(id) {
+  return checkItemExists("users", "username", id).then(() => {
+    return db
+      .query("SELECT * FROM users WHERE username = $1", [id])
+      .then(({ rows }) => {
+        return rows[0];
+      });
+  });
+}
+
+module.exports = { fetchAllUsers, fetchUserByUsername };
--- routes/users-router.js
+++ routes/users-router.js
@@ -1,9 +1,11 @@
 const express = require("express");
 
-const { getAllUsers } = require("../controllers/users.controller");
+const { getAllUsers, getUserByUsername } = require("../controllers/users.controller");
 
 const usersRouter = express.Router();
 
 usersRouter.get("/", getAllUsers);
+usersRouter.get("/:username", getUserByUsername);
+
 
 module.exports = { usersRouter };
